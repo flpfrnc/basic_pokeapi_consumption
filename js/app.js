98c7f45a8e0c8d6b88a26d9ddb5a7b1dc4de5fd7
Felipe França <felipe.franca.108@ufrn.edu.br>
2022-02-16 18:04:06 -0300

    add functionalities

--- js/app.js
+++ js/app.js
@@ -1,103 +1,169 @@
 const fetchPokemon = () => {
-    retrievePokemon();
-  };
-  
-  function retrievePokemon() {
-    for (let i = 1; i <= 150; i++) {
-      var pokeRequest = $.ajax({
-        url: `https://pokeapi.co/api/v2/pokemon/${i}`,
-        type: "GET"
-      });
-  
-      pokeRequest.done(function (response) {
-      
-        let pokemons = document.querySelector("#poke__container");
-        let pokemon_card = document.createElement("div");
-        let pokemon_image_container = document.createElement("div");
-        let pokemon_image = document.createElement("img")
-        let pokemon_like_container = document.createElement("div")
-        let pokemon_like = document.createElement("i")
-        let pokemon_name = document.createElement("h4");
-        let separator = document.createElement("hr");
-        
-        // setting up attributes
+  retrievePokemon();
+};
 
-        // pokemon card
-        pokemon_card.classList.add("card_pokemon")
-        
-        // pokemon like container
-        pokemon_like_container.classList.add("pokeLike__container")
-        
-        // pokemon image container
-        pokemon_image_container.classList.add("pokeImage__container")
+function retrievePokemon() {
+  for (let i = 1; i <= 150; i++) {
+    var pokeRequest = $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon/${i}`,
+      type: "GET"
+    });
 
-        // pokemon image
-        pokemon_image.setAttribute('src', `${response.sprites.other["official-artwork"].front_default}`)
-        pokemon_image.setAttribute('width', '100');
-        pokemon_image.setAttribute('height', '100');
-        
-        // pokemon like
-        if(localStorage.getItem(response.name) == 'liked'){
-            pokemon_like.classList.add("fa-solid");
-            pokemon_like.classList.add("fa-heart");
-            pokemon_like.classList.add("likable");    
-        }else{
-            pokemon_like.classList.add("far");
-            pokemon_like.classList.add("fa-heart");
-            pokemon_like.classList.add("likable");
-        }
-        pokemon_like.setAttribute('id', `${response.name}`);
+    pokeRequest.done(function (response) {
+
+      let pokemons = document.querySelector("#poke__container");
+      let pokemon_card = document.createElement("div");
+      let pokemon_image_container = document.createElement("div");
+      let pokemon_image = document.createElement("img")
+      let pokemon_like_container = document.createElement("div")
+      let pokemon_like = document.createElement("i")
+      let pokemon_name = document.createElement("h4");
+      let separator = document.createElement("hr");
+
+      // setting up attributes
+
+      // pokemon card
+      pokemon_card.classList.add("card_pokemon")
+
+      // pokemon like container
+      pokemon_like_container.classList.add("pokeLike__container")
+
+      // pokemon image container
+      pokemon_image_container.classList.add("pokeImage__container")
+
+      // pokemon image
+      pokemon_image.setAttribute('src', `${response.sprites.other["official-artwork"].front_default}`)
+      pokemon_image.setAttribute('width', '100');
+      pokemon_image.setAttribute('height', '100');
+
+      // pokemon like
+      if (localStorage.getItem(response.name) == 'liked') {
+        pokemon_like.classList.add("fa-solid");
+        pokemon_like.classList.add("fa-heart");
+        pokemon_like.classList.add("likable");
+      } else {
+        pokemon_like.classList.add("far");
+        pokemon_like.classList.add("fa-heart");
+        pokemon_like.classList.add("likable");
+      }
+      pokemon_like.setAttribute('id', `${response.name}`);
 
 
-        // pokemon name
-        pokemon_name.classList.add("text-center")
-        
+      // pokemon name
+      pokemon_name.classList.add("text-center")
 
-        // setting up children
 
-        pokemon_image_container.appendChild(pokemon_image)
-        pokemon_image_container.appendChild(pokemon_like_container)
-        pokemon_like_container.appendChild(pokemon_like)
+      // setting up children
 
-        pokemon_name.innerText = response.name
-        
-        pokemon_card.appendChild(pokemon_image_container)
-        pokemon_card.appendChild(separator)
-        pokemon_card.appendChild(pokemon_name)
-        
-        pokemons.appendChild(pokemon_card)
-        
+      pokemon_image_container.appendChild(pokemon_image)
+      pokemon_image_container.appendChild(pokemon_like_container)
+      pokemon_like_container.appendChild(pokemon_like)
 
-        let likes = document.getElementsByClassName('likable')
-        
-        if(likes.length == 150){
-            for(var i=0;i<likes.length;i++){ 
-                likes[i].addEventListener("click", function(){
-                    if(this.classList.contains("far")) {
-                        this.classList.remove("far")
-                        this.classList.remove("fa-heart")
-                        this.classList.add("fa-solid")
-                        this.classList.add("fa-heart")
+      pokemon_name.innerText = response.name
 
-                        localStorage.setItem( this.id, 'liked')
-                    }else {
-                        this.classList.remove("fa-solid")
-                        this.classList.remove("fa-heart")
-                        this.classList.add("far")
-                        this.classList.add("fa-heart")
+      pokemon_card.appendChild(pokemon_image_container)
+      pokemon_card.appendChild(separator)
+      pokemon_card.appendChild(pokemon_name)
 
-                        localStorage.removeItem(this.id)
-                    }
-                }); 
+      pokemons.appendChild(pokemon_card)
+
+
+      let likes = document.getElementsByClassName('likable')
+
+      if (likes.length == 150) {
+        for (var i = 0; i < likes.length; i++) {
+          likes[i].addEventListener("click", function () {
+            if (this.classList.contains("far")) {
+              this.classList.remove("far")
+              this.classList.remove("fa-heart")
+              this.classList.add("fa-solid")
+              this.classList.add("fa-heart")
+
+              localStorage.setItem(this.id, 'liked')
+              addpokemon()
+              location.reload()
+            } else {
+              this.classList.remove("fa-solid")
+              this.classList.remove("fa-heart")
+              this.classList.add("far")
+              this.classList.add("fa-heart")
+
+              localStorage.removeItem(this.id)
+              addpokemon()
+              location.reload()
             }
+          });
         }
-      });
-    }
+      }
+    });
   }
-  
-  fetchPokemon();
+}
 
-  setInterval(() => {
-    let liked = localStorage
-    console.log(liked)
-  }, 1000);
+fetchPokemon();
+
+
+function retrieveAllStorage() {
+  let localStorageItems = []
+  let keys = Object.keys(localStorage);
+  let i = keys.length;
+  while (i--) {
+    localStorageItems.push(keys[i]);
+  }
+  return localStorageItems;
+}
+
+
+function removePokemon(pokemon){
+  localStorage.removeItem(pokemon)
+}
+
+
+function addpokemon() {
+  let favoriteInfo = document.getElementsByClassName('favorite__Info')[0];
+  let favoriteInfoResponsive = document.getElementsByClassName('favorite__InfoResponsive')[0];
+
+  let favorites = retrieveAllStorage()  
+  favoriteInfo.innerHTML = ''
+  favoriteInfoResponsive.innerHTML = ''
+
+  favorites.forEach(element => {
+    let favorite_container = document.createElement("div")
+    favorite_container.classList.add("favorite__container")
+    let linkPokemon = document.createElement("button")
+    linkPokemon.classList.add('delete-pokemon')
+    let deletePokemon = document.createElement("i")
+    deletePokemon.classList.add('fas')
+    deletePokemon.classList.add('fa-trash')
+    deletePokemon.classList.add('delete-pokemon')
+    linkPokemon.appendChild(deletePokemon)
+  
+    let strongPokemon = document.createElement('b')
+    strongPokemon.innerText = element
+
+
+    favorite_container.appendChild(strongPokemon)
+    favorite_container.appendChild(linkPokemon)
+
+    let copy_favorite_container = favorite_container.cloneNode(true)
+
+    favoriteInfo.appendChild(favorite_container)
+    favoriteInfoResponsive.appendChild(copy_favorite_container)
+  })
+}
+
+addpokemon()
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  let deletar = document.getElementsByClassName('delete-pokemon')
+  
+  for(let i = 0; i < deletar.length; i++){
+    deletar[i].addEventListener('click', function(){
+      localStorage.removeItem(deletar[i].parentElement.parentElement.firstChild.innerText)
+      addpokemon()
+      location.reload()
+    })
+  }
+});
+
+
+
